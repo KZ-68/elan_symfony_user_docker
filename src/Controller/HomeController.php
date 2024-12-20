79e320ea2e281e80607ee19066fe8c57486a4566
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-12-20 01:57:00 +0100

    refactor: update route url and name for home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/', name: 'app_')]
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('home', name: 'home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
